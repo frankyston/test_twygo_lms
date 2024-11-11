690a20b16400e4230f47b616678f211d67bfeaac
Frankyston Lins <frankyston@gmail.com>
2024-11-11 19:21:13 -0300

    Show courses in home and my courses

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,11 @@ class HomeController < ApplicationController
   layout 'internal'
 
   def index
-    @courses = Course.active
+    @courses = Course.active.order(created_at: :desc)
+    if params[:my_courses]
+      @courses = @courses.where(id: Enrollment.where(user_id: @current_user.id).pluck(:course_id)).order(created_at: :desc)
+    else
+      @courses = @courses.where.not(id: Enrollment.where(user_id: @current_user.id).pluck(:course_id)).order(created_at: :desc)
+    end
   end
 end
