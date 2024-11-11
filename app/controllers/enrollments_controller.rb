ac3a0df2d2d7b716805211a200b65782e197013d
Frankyston Lins <frankyston@gmail.com>
2024-11-11 19:20:55 -0300

    Bugfix redirect after enrollment of course

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -3,13 +3,13 @@ class EnrollmentsController < ApplicationController
     @course = Course.find(params[:course_id])
     if @course
       if @current_user.my_courses.include?(@course)
-        redirect_to course_path(@course), alert: "Você já está inscrito neste curso!"
+        redirect_to course_lesson_path(@course, @course.lessons.first), alert: "Você já está inscrito neste curso!"
       else
         @enrollment = @current_user.enrollments.create(course: @course)
-        redirect_to course_path(@course), notice: "Você se inscreveu no curso com sucesso!"
+        redirect_to course_lesson_path(@course, @course.lessons.first), notice: "Você se inscreveu no curso com sucesso!"
       end
     else
-      redirect_to course_path(@course), alert: "Curso não encontrado!"
+      redirect_to course_lesson_path(@course, @course.lessons.first), alert: "Curso não encontrado!"
     end
   end
 end
