1b4482f40ea65a543f8ef51cd0d82d4ba25d3fe6
Frankyston Lins <frankyston@gmail.com>
2024-11-11 19:20:13 -0300

    Improve seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,22 @@
 ## Users
 
-User.find_or_create_by(email: 'admin@example.com', password: '123456', password_confirmation: '123456', role: 'admin')
+User.find_or_create_by(email: 'admin@example.com', role: 'admin') do |user|
+  user.password = '123456'
+  user.password_confirmation = '123456'
+end
 
 ## Courses
 
-Course.find_or_create_by(title: 'Curso de Ruby on Rails', description: 'Curso de Ruby on Rails', instructor: 'Teste', started_at: Date.today, ended_at: Date.today + 1.month, user: User.first)
+Course.find_or_create_by(
+  title: 'Curso de Ruby on Rails',
+  description: 'Curso de Ruby on Rails',
+  instructor: 'Teste',
+  started_at: Date.today,
+  ended_at: Date.today + 1.month,
+  user: User.first
+) do |course|
+  course.cover = File.open(Rails.root.join('app', 'assets', 'images', 'cover.jpg'))
+end
 
 ## Lessons
 
@@ -13,7 +25,8 @@ Lesson.find_or_create_by(
   description: 'Aula 1',
   duration: 4,
   file_size: 8503379,
-  course: Course.first,
-  file: File.open(Rails.root.join('app', 'assets', 'videos', 'sample.mp4')),
-  thumbnail: File.open(Rails.root.join('app', 'assets', 'images', 'thumbnail.png'))
-)
+  course: Course.first
+) do |lesson|
+  lesson.file = File.open(Rails.root.join('app', 'assets', 'videos', 'sample.mp4'))
+  lesson.thumbnail = File.open(Rails.root.join('app', 'assets', 'images', 'thumbnail.png'))
+end
