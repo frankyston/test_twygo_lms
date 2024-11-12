2a9df9752952c952683da8ddf8018c7a49e1d6a2
Frankyston Lins <frankyston@gmail.com>
2024-11-12 13:50:59 -0300

    Improve seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,34 +1,49 @@
 ## Users
 
-User.find_or_create_by(email: 'admin@example.com', role: 'admin') do |user|
+User.find_or_create_by(email: 'student@student.com', role: 'student') do |user|
+  user.password = '123456'
+  user.password_confirmation = '123456'
+end
+
+User.find_or_create_by(email: 'admin@admin.com', role: 'admin') do |user|
   user.password = '123456'
   user.password_confirmation = '123456'
 end
 
 ## Courses
 
-Course.find_or_create_by(
-  title: 'Curso de Ruby on Rails',
-  description: 'Curso de Ruby on Rails',
-  short_description: 'Curso de Ruby on Rails',
-  instructor: 'Teste',
-  started_at: Date.today,
-  ended_at: Date.today + 1.month,
-  user: User.first
-) do |course|
-  course.cover = File.open(Rails.root.join('test', 'fixtures', 'files', 'cover.jpg'))
+5.times do |i|
+  covers = ['cover.jpg', 'cover2.png']
+  Course.find_or_create_by(
+    title: "Curso ##{i}",
+    description: "Curso ##{i}",
+    short_description: "Curso ##{i}",
+    instructor: 'Teste',
+    started_at: Date.today,
+    ended_at: Date.today + 1.month,
+    user: User.first
+    ) do |course|
+      course.cover = File.open(Rails.root.join('test', 'fixtures', 'files', covers.sample))
+    end
 end
 
 ## Lessons
 
-Lesson.find_or_create_by(
-  title: 'Aula 1',
-  description: 'Aula 1',
-  short_description: 'Aula 1',
-  duration: 4,
-  file_size: 8503379,
-  course: Course.first
-) do |lesson|
-  lesson.file = File.open(Rails.root.join('test', 'fixtures', 'files', 'sample.mp4'))
-  lesson.thumbnail = File.open(Rails.root.join('test', 'fixtures', 'files', 'thumbnail.png'))
+files = ['sample.mp4', 'sample2.mp4']
+thumbnails = ['thumb1.png', 'thumb2.jpg']
+
+Course.all.each do |course|
+  5.times do |i|
+    Lesson.find_or_create_by(
+      title: "Aula ##{i}",
+      description: "Aula ##{i}",
+      short_description: "Aula ##{i}",
+      duration: 4,
+      file_size: 8503379,
+      course: course
+    ) do |lesson|
+      lesson.file = File.open(Rails.root.join('test', 'fixtures', 'files', files.sample))
+      lesson.thumbnail = File.open(Rails.root.join('test', 'fixtures', 'files', thumbnails.sample))
+    end
+  end
 end
