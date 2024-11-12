69559072681920a80084670dca88758b6bf2d197
Frankyston Lins <frankyston@gmail.com>
2024-11-12 13:12:45 -0300

    Add test enrollment

--- test/models/enrollment_test.rb
+++ test/models/enrollment_test.rb
@@ -1,7 +1,33 @@
 require "test_helper"
 
 class EnrollmentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should be invalid" do
+    enrollment = Enrollment.new
+    assert_not enrollment.valid?
+  end
+
+  test "should not be valid without user" do
+    enrollment = Enrollment.new(user: nil)
+    assert_not enrollment.valid?
+  end
+
+  test "should not be valid without course" do
+    enrollment = Enrollment.new(course: nil)
+    assert_not enrollment.valid?
+  end
+
+  test "should be valid" do
+    enrollment = enrollments(:one)
+    assert enrollment.valid?
+  end
+
+  test "should return the correct course" do
+    enrollment = enrollments(:one)
+    assert_equal courses(:one), enrollment.course
+  end
+
+  test "should return the correct user" do
+    enrollment = enrollments(:one)
+    assert_equal users(:user_one), enrollment.user
+  end
 end
