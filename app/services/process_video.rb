97057c710b8dc39ef4afded88e3d097d02405d5c
Frankyston Lins <frankyston@gmail.com>
2024-11-13 08:22:21 -0300

    Adjustment Process video on production

--- app/services/process_video.rb
+++ app/services/process_video.rb
@@ -1,4 +1,6 @@
 require "streamio-ffmpeg"
+require 'open-uri'
+require 'fileutils'
 
 class ProcessVideo
   def initialize(lesson)
@@ -6,22 +8,57 @@ class ProcessVideo
   end
 
   def call
-    set_duration
-    set_file_size
-    @lesson.save
+    if Rails.env.production?
+      download_and_process
+    else
+      process_local
+    end
   end
 
   private
 
-  def file_path
-    ActiveStorage::Blob.service.send(:path_for, @lesson.file.key)
+  def download_and_process
+    create_temp_file
+    process_video(@temp_file_path)
+  ensure
+    cleanup_temp_file
   end
 
-  def set_duration
-    @lesson.duration = (::FFMPEG::Movie.new(file_path).duration / 60).round(2)
+  def process_local
+    file_path = ActiveStorage::Blob.service.send(:path_for, @lesson.file.key)
+    process_video(file_path)
+  end
+
+  def process_video(path)
+    set_duration(path)
+    set_file_size
+    @lesson.save
+  end
+
+  def create_temp_file
+    FileUtils.mkdir_p(Rails.root.join('tmp', 'videos'))
+    @temp_file_path = Rails.root.join('tmp', 'videos', "#{@lesson.file.key}.mp4").to_s
+    download_from_s3
+    @temp_file_path
+  end
+
+  def download_from_s3
+    url = @lesson.file.url
+    File.open(@temp_file_path, 'wb') do |file|
+      file << URI.open(url).read
+    end
+  end
+
+  def set_duration(path)
+    movie = ::FFMPEG::Movie.new(path)
+    @lesson.duration = (movie.duration / 60).round(2)
   end
 
   def set_file_size
     @lesson.file_size = @lesson.file.byte_size
+  end
+
+  def cleanup_temp_file
+    File.delete(@temp_file_path) if File.exist?(@temp_file_path)
   end
 end
